98ed43913ce00707b39278479a5d5b2dd7b9ddfd
Akanksha <akankshakotain568@gmail.com>
2021-06-22 22:39:30 +0530

    First commit

---
+++ app/src/main/java/com/example/myprofile/skills/dashboard.java
@@ -0,0 +1,16 @@
+package com.example.myprofile.skills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myprofile.R;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
